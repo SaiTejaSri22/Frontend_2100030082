8575c5a10ed38a6b6f853f572e43194f60891d33
SaiTejaSri22 <144007194+SaiTejaSri22@users.noreply.github.com>
2024-05-18 20:05:11 +0530

    Dynamic Form Generation

---
+++ dynamic-form/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DynamicForm from './components/DynamicForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dynamic Form Builder</h1>
+      <DynamicForm />
+    </div>
+  );
+}
+
+export default App;
